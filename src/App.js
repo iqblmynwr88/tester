9880f12db8a0eb3d5bb19cb4a6334941785048b9
iqblmynwr88 <hexawolf88@gmail.com>
2023-03-20 15:28:31 +0700

    added footer

--- src/App.js
+++ src/App.js
@@ -17,6 +17,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faCheckSquare, faCoffee, fab)
+
 function App() {
   return (
     <div className="App2">
@@ -45,7 +52,7 @@ function App() {
           <Row>
             <Col xs="12" lg="6">
               <h3>Hi, there 👋🎉</h3>
-              <p>welcome to our <b>"home"</b>, yes we may call it home or whatever you want to call it. 🥰🫰</p>
+              <p>welcome to our <bold>space</bold>, yes we may call it home or whatever you want to call it. 🥰🫰</p>
               <br/>
               <p>Our house is still in the process of being repaired, there are a few things we have to add. so we apologize if there are some features that are still not working properly.</p>
             </Col>
@@ -57,7 +64,7 @@ function App() {
           </Row>
           <Row>
             <Col className='hexa-text-center'>
-              <h3>How to stake in our validator?</h3>
+              <h3>How to <bold>stake</bold> in our validator?</h3>
               <br/>
               <p>Hexawolf88 Validators helps investors compound their crypto investments with low commission and advanced support</p>
               <br/>
@@ -67,25 +74,25 @@ function App() {
           <Row className='blank-hexa'>
           </Row>
           <Row>
-            <Col xs lg='12' className='text-center'>
+            <Col xs='12' sm='12' md='12' lg='12' className='text-center'>
               <h3>Why Stake With US</h3>
             </Col>
-            <Col xs='12' lg='3'>
+            <Col xs='12' md='6' lg='3'>
               <Col className='box-10'>
                 <img src={icon_secure} className='logo-box' alt="logo" /><span className='span-logo'>Service is Highly secured with 24/7 Monitoring.</span>
               </Col>
             </Col>
-            <Col xs='12' lg='3'>
+            <Col xs='12' md='6' lg='3'>
               <Col className='box-10'>
                 <img src={icon_edge} className='logo-box' alt="logo" /><span className='span-logo'>Fast and Reliable Explorer between chains.</span>
               </Col>
             </Col>
-            <Col xs='12' lg='3'>
+            <Col xs='12' md='6' lg='3'>
               <Col className='box-10'>
                 <img src={icon_website} className='logo-box' alt="logo" /><span className='span-logo'>Provide Public Endpoints for Communities.</span>
               </Col>
             </Col>
-            <Col xs='12' lg='3'>
+            <Col xs='12' md='6' lg='3'>
               <Col className='box-10'>
                 <img src={icon_money} className='logo-box' alt="logo" /><span className='span-logo'>Low Comission and Impressive Uptime with 0 Slashing!.</span>
               </Col>
@@ -106,6 +113,17 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer className='footer-hexa'>
+        <Col xs='12' md='6' lg='6'>
+          <bold>Hexawolf88 Space © - All rights reserved</bold>
+        </Col>
+        <br/>
+        <Col xs='12' md='6' lg='6'>
+          <a href='https://github.com/iqblmynwr88' className='App-link btn btn-outline-light' target='_blank' rel='nooper noreferrer'><FontAwesomeIcon icon={["fab", "github"]} /></a>
+          
+          <a href='https://twitter.com/IqbalMaulaY' className='App-link btn btn-outline-light' target='_blank' rel='nooper noreferrer'><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
+        </Col>
+      </footer>
     </div>
   );
 }
